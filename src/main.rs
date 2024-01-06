6052f60eb62e4a9cd463f97caa13b9caa11f1d83
Vishal Patil <vishpat@gmail.com>
2024-01-05 22:13:57 -0500

    Checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use pollster::block_on;
 
-const WIDTH: u32 = 1024;
-const HEIGHT: u32 = 1024;
+const WIDTH: usize = 1024;
+const HEIGHT: usize = 1024;
 
 async fn gpu_device_queue() -> (wgpu::Device, wgpu::Queue) {
     let instance = wgpu::Instance::default();
@@ -19,12 +19,10 @@ async fn gpu_device_queue() -> (wgpu::Device, wgpu::Queue) {
 }
 
 async fn compute_shader(device: &wgpu::Device) -> wgpu::ShaderModule {
-
     device.create_shader_module(wgpu::ShaderModuleDescriptor {
-        label: Some("Compute Shader"),
+        label: Some("MandelBrot Fragment Shader"),
         source: wgpu::ShaderSource::Wgsl(include_str!("mandelbrot.wgsl").into()),
     })
-
 }
 
 async fn gpu_buffer(device: &wgpu::Device, size: u64) -> wgpu::Buffer {
@@ -39,9 +37,50 @@ async fn gpu_buffer(device: &wgpu::Device, size: u64) -> wgpu::Buffer {
 async fn run() {
     let (device, queue) = gpu_device_queue().await;
 
-    let cs_module = compute_shader(&device).await; 
+    let cs_module = compute_shader(&device).await;
 
-    let storage_buffer = gpu_buffer(&device, (WIDTH * HEIGHT * 4) as u64).await;
+    let size = (WIDTH * HEIGHT * std::mem::size_of::<f32>()) as u64;
+    let storage_buffer = gpu_buffer(&device, size).await;
+
+    let compute_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label: Some("MandelBrot Compute Pipeline"),
+        layout: None,
+        module: &cs_module,
+        entry_point: "main",
+    });
+
+    let bind_group_layout = compute_pipeline.get_bind_group_layout(0);
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: None,
+        layout: &bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: storage_buffer.as_entire_binding(),
+        }],
+    });
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Command Encoder"),
+    });
+
+    {
+        let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some("Compute Pass"),
+            timestamp_writes: None
+        });
+
+        cpass.set_pipeline(&compute_pipeline);
+        cpass.set_bind_group(0, &bind_group, &[]);
+        cpass.insert_debug_marker("MandelBrot Compute Pass");
+        cpass.dispatch_workgroups((WIDTH * HEIGHT) as u32, 1, 1);
+    }
+
+    queue.submit(Some(encoder.finish()));
+    let buffer_slice = storage_buffer.slice(..);
+    let (sender, receiver) = flume::bounded(1);
+    let buffer_future = buffer_slice.map_async(wgpu::MapMode::Read, move |v| sender.send(v).unwrap());
+    device.poll(wgpu::Maintain::Wait);
+    
 }
 
 fn main() {
